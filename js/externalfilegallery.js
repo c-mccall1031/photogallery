ae4f2073f87b515451334e82813360f82a494a95
c-mccall1031 <148171566+c-mccall1031@users.noreply.github.com>
2023-11-20 20:07:06 -0800

    Update externalfilegallery.js

--- js/externalfilegallery.js
+++ js/externalfilegallery.js
@@ -1,1 +1,13 @@
+<script>
+function bigImg(x) {
+   x.style.height = "64px";
+   x.style.width = "64px";
+}
+
+function normalImg(x) {
+   x.style.height = "32px";
+   x.style.width = "32px";
+}
+</script>
+  
 <img onmouseover="bigImg(this)" onmouseout="normalImg(this)" border="0" src="images/Axis_in_snow.jpg" alt="My dog hiking in the snow" width="32" height="32">
